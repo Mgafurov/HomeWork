30cc8f0fa4c8c68f294146d4698990729dfec42e
Малик <poseydon100500@gmail.com>
2023-05-22 04:24:00 +0500

    task8

---
+++ HomeWork_Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число N:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Чётные числа от 1 до " + n + ":");
+
+        for (int i = 2; i <= n; i += 2)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
