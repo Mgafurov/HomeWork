ed0db91ef64ca5553ebeaa148caad4e05a8314ba
Малик <poseydon100500@gmail.com>
2023-05-25 21:11:29 +0500

    add commit

---
+++ HomeWork_Task12/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Введите цифру обозначающий день недели");
+        int dayOfWeek = int.Parse(Console.ReadLine());
+        //Проверяем является ли день выходным
+        bool isWeekend = (dayOfWeek == 6) || (dayOfWeek == 7);
+
+        if(isWeekend)
+        {
+            System.Console.WriteLine("Да это выходной день");
+        }
+        else
+        {
+            System.Console.WriteLine("Нет,это не выходной день");
+        }
+    }
+}
