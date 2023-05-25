3e66708a5660ccfcaa25186e35f410ee808bddf2
Малик <poseydon100500@gmail.com>
2023-05-25 20:51:55 +0500

    Task10

---
+++ HomeWork_Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Введите трёхзначное число");
+        int number = int.Parse(Console.ReadLine());
+        //получаем вторую цифру
+        int secondDigit=(number/10%10);
+        System.Console.WriteLine("Вторая цифра числа " + secondDigit);
+    }
+}
