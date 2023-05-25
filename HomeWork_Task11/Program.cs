f191c99710ddb600d089bfe429ba72d6505d9625
Малик <poseydon100500@gmail.com>
2023-05-25 21:11:10 +0500

    add commit

---
+++ HomeWork_Task11/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        int number = int.Parse(Console.ReadLine());
+
+        // Получаем третью цифру числа
+        int thirdDigit = (number / 100) % 10;
+
+        if (thirdDigit != 0)
+        {
+            Console.WriteLine("Третья цифра числа: " + thirdDigit);
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет.");
+        }
+    }
+}
