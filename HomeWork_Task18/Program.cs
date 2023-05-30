e2a219f4638e2fee9e66a0fba26230b94d000902
Малик <poseydon100500@gmail.com>
2023-05-30 13:46:52 +0500

    comm

---
+++ HomeWork_Task18/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int product = CalculateProduct(n);
+        System.Console.WriteLine("Произведение чисел от 1 до N: " + product);
+    }
+
+    static int CalculateProduct(int n)
+    {
+        int product = 1;
+        for(int i =1; i<= n; i++)
+        {
+            product *= i;
+        }
+        return product;
+    }
+}
