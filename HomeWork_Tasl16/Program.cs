f3f11d8236b7fd023a6e92f925b35665f4132d3c
Малик <poseydon100500@gmail.com>
2023-05-30 13:47:35 +0500

    add

---
+++ HomeWork_Tasl16/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Summa(int num)
+{
+    int result = 0;
+    for (int i=1; i<=num; i++)
+        result+=i;
+    return result;
+}
+
+void Summa2(int num)
+{
+    int result = 0;
+    for (int i=1; i<=num; i++)
+        result+=i;
+    System.Console.WriteLine($"Сумма всех чисел от 1 до {num} равна {result}");
+}
+
+
+Console.Clear();
+System.Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма всех чисел от 1 до {num} равна {Summa(num)}");
+Summa2(num);
