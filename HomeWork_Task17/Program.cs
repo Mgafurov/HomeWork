e7d6931b19ada86999c1a54659950bc270908f98
Малик <poseydon100500@gmail.com>
2023-05-30 13:46:26 +0500

    commit

---
+++ HomeWork_Task17/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+
+        int digitCount = input.Length;
+        Console.WriteLine("Количество цифр в числе: " + digitCount);
+    }
+}
