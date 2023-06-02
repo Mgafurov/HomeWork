f844eb360b9a6c7b489215fd4d6bf19e20f13a12
Малик <poseydon100500@gmail.com>
2023-06-02 10:35:44 +0500

    Task21

---
+++ HomeWork_Task21/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Введите число");
+        int number = int.Parse(Console.ReadLine());
+        int sum = CalculateDigitSum(number);
+        System.Console.WriteLine("Сумма цифр числа" + sum);
+    }
+    static int CalculateDigitSum(int number)
+    {
+        int sum = 0;
+        while(number > 0)
+        {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
+}
