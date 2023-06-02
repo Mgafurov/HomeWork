2fdefeac6f4d5af4861cba928e9d777e862aa768
Малик <poseydon100500@gmail.com>
2023-06-02 15:10:26 +0500

    Task24

---
+++ HomeWork_Task24/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем генератор случайных чисел
+        Random random = new Random();
+
+        // Создаем массив и заполняем его случайными положительными трехзначными числами
+        int[] numbers = new int[4];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+        }
+
+        // Подсчитываем количество четных чисел в массиве
+        int evenCount = 0;
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+        // Выводим результат
+        Console.WriteLine("Исходный массив: [" + string.Join(", ", numbers) + "]");
+        Console.WriteLine("Количество четных чисел: " + evenCount);
+    }
+}
