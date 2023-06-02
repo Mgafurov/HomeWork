130a4118f8a080102bc407f24e11a637701d4c9e
Малик <poseydon100500@gmail.com>
2023-06-02 15:11:22 +0500

    Task26

---
+++ HomeWork_Task26/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] numbers = {3.14, -2.5, 10.8, 5.2, 7.9};
+        double max = double.MinValue;
+        double min = double.MaxValue;
+        foreach(double number in numbers)
+        {
+            if(number>max)
+            {
+                max=number;
+            }
+            if(number<min)
+            {
+                min=number;
+            }
+        }
+        double diffence = max - min;
+        System.Console.WriteLine("Исходный массив: ["+string.Join(",", numbers)+"]");
+        System.Console.WriteLine("Максимальный элемент" + max);
+        System.Console.WriteLine("Минимальный элемент" +  min);
+        System.Console.WriteLine("Разница между максимальным и минимальным элементами :" + diffence);
+    }
+}
